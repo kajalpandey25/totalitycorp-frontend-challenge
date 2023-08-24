8a679445ec7540b141edc0a4531be0b4d7b19a62
Kajal Pandey <119345768+sp8172975@users.noreply.github.com>
2023-08-24 23:28:35 +0530

    ok

--- react-app/src/About.js
+++ react-app/src/About.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import HeroSection from "./components/HeroSection";
+import { useProductContext } from "./context/productcontex";
 
 const About = () => {
+  const { myName } = useProductContext();
+
+  const data = {
+    name: "Kajal Ecommerce",
+  };
+
   return (
-    <div>
-      About
-    </div>
-  )
-}
+    <>
+      {myName}
+      <HeroSection myData={data} />
+    </>
+  );
+};
 
 export default About;
